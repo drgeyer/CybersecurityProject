9825defd0d8d4f63826f4d2471053f27eb97cae4
Dylan Geyer <dylan.geyer@mines.sdsmt.edu>
2015-03-31 16:42:38 -0600

    NewPasswordPanel has a button, and textField

--- CybersecurityProject/src/cybersecurityproject/NewPasswordPanel.java
+++ CybersecurityProject/src/cybersecurityproject/NewPasswordPanel.java
@@ -4,16 +4,20 @@
  * and open the template in the editor.
  */
 package cybersecurityproject;
+import java.awt.GridLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.*;
 
 /**
  *
  * @author 7051665
  */
-public class NewPasswordPanel extends JPanel {
+public class NewPasswordPanel extends JPanel implements KeyListener{
     
     public NewPasswordPanel()
     {
+        this.setLayout(new GridLayout(3, 1));
         initComponents();
         this.setVisible(true);
         this.updateUI();
@@ -21,10 +25,31 @@ public class NewPasswordPanel extends JPanel {
     
     private void initComponents()
     {
-        test = new JLabel("New Password Pane");
+        test = new JLabel("Begin typing to create a new password");
+        passwordTextBox = new JTextField();
+        savePassword = new JButton("Save Password");
         add(test);
+        add(passwordTextBox);
+        add(savePassword);
     }
     
     //User defined variables
     JLabel test;    
+    JTextField passwordTextBox;
+    JButton savePassword;
+
+    @Override
+    public void keyTyped(KeyEvent ke) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
